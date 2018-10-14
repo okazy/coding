90ce37b2cacb3a967fb3aae3a540d8c0b5e24264
Hideki Okajima <hideki_okajima@lockon.co.jp>
2018-10-14 23:02:33 +0900

    add simple-array-sum

---
+++ ranks/simple-array-sum.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * Complete the simpleArraySum function below.
+ */
+function simpleArraySum($ar) {
+    $sum = 0;
+    foreach ($ar as $value) {
+        $sum += $value;
+    }
+    return $sum;
+}
+
+$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+$stdin = fopen("php://stdin", "r");
+
+fscanf($stdin, "%d\n", $ar_count);
+
+fscanf($stdin, "%[^\n]", $ar_temp);
+
+$ar = array_map('intval', preg_split('/ /', $ar_temp, -1, PREG_SPLIT_NO_EMPTY));
+
+$result = simpleArraySum($ar);
+
+fwrite($fptr, $result . "\n");
+
+fclose($stdin);
+fclose($fptr);
