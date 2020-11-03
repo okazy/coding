2c268ee16807afdf752f6b270605de7b08eb8623
okazy <hideki518c@gmail.com>
2020-11-03 20:41:43 +0900

    NumberOfDiscIntersections

--- dility/NumberOfDiscIntersections.php
+++ dility/NumberOfDiscIntersections.php
@@ -4,7 +4,7 @@ function solution($A) {
     $count = 0;
     foreach (range(0, count($A)-2) as $keyJ) {
         foreach (range($keyJ+1, count($A)-1) as $keyK) {
-            if (($keyJ + $A[$keyJ]) > $keyK - $A[$keyK]) {
+            if (($keyJ + $A[$keyJ]) >= $keyK - $A[$keyK]) {
                 $count++;
             }
         }
